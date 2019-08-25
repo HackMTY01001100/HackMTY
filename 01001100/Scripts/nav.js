d736c1798f0ef011f7d87bdd09aa70e30bb02c30
Luis Fernández <luiggi@fernandez.org.mx>
2019-08-25 10:05:05 -0500

    c

--- 01001100/Scripts/nav.js
+++ 01001100/Scripts/nav.js
@@ -26,11 +26,9 @@ function stickyNav() {
 }
 
 var a=document.getElementsByTagName("a");
-for(var i=0;i<a.length;i++)
-{
-    a[i].onclick=function()
-    {
+for(var i=0;i<a.length;i++){
+    a[i].onclick=function(){
         window.location=this.getAttribute("href");
-        return false
+        return false;
     }
 }
