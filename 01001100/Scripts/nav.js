df732de100484a4f9da1bb15285fcba5a83ecd7d
Luis Fernández <luiggi@fernandez.org.mx>
2019-08-24 20:27:13 -0500

    s

---
+++ 01001100/Scripts/nav.js
@@ -0,0 +1,26 @@
+function navHide() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navigation") {
+    x.className += " responsive";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML  = "";
+  } else {
+    x.className = "navigation";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML = "";
+    document.getElementById("").innerHTML  = "";
+  }
+}
+window.onscroll = function() {stickyNav()};
+function stickyNav() {
+  var nav = document.getElementById("navbar");
+  var sticky = nav.offsetTop;
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("navigation");
+  } else {
+    nav.classList.remove("navigation");
+  }
+}
